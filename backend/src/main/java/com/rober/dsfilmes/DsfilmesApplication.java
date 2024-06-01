1dddc1d88be9757d200b39e87daac9189f2f2580
Roberval Silva <jroberval75@gmail.com>
2024-06-01 13:17:57 -0300

    Primeiro projeto

---
+++ backend/src/main/java/com/rober/dsfilmes/DsfilmesApplication.java
@@ -0,0 +1,13 @@
+package com.rober.dsfilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsfilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsfilmesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rober/dsfilmes/DsfilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rober.dsfilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsfilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
